f578c5adccad0b34eec6a7d2fb7f27dd2bc3107d
Dallas Reedy <code@dallasreedy.com>
2010-03-17 20:57:17 -0700

    added the Game, GamePlay, Round and Score models

---
+++ db/migrate/20100318035459_game_play.rb
@@ -0,0 +1,7 @@
+class GamePlay < ActiveRecord::Migration
+  def self.up
+  end
+
+  def self.down
+  end
+end
---
+++ db/migrate/20100318035548_create_scores.rb
@@ -0,0 +1,14 @@
+class CreateScores < ActiveRecord::Migration
+  def self.up
+    create_table :scores do |t|
+      t.integer :round_id
+      t.integer :player_id
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :scores
+  end
+end
