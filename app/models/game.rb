e2fe8424081efddf2870fec2f16528cad3a4a44c
Dallas Reedy <code@dallasreedy.com>
2010-03-21 16:31:51 -0700

    re-annotated the Game model with the correct table information

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,11 @@
 # == Schema Information
-# Schema version: 20100318035548
+# Schema version: 20100318035409
 #
 # Table name: games
 #
 #  id         :integer         not null, primary key
+#  name       :string(255)
+#  slug       :string(255)
 #  created_at :datetime
 #  updated_at :datetime
 #
