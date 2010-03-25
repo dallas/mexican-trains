6621f9cfc455808aefdda3857b9b49362d07c16c
Dallas Reedy <code@dallasreedy.com>
2010-03-25 08:41:20 -0700

    couldn’t accurately grab the BoardGameGeek site, so I ended up just copying the source I wanted and loading that instead

--- nokogiri_test.rb
+++ nokogiri_test.rb
@@ -1,15 +1,11 @@
 require 'rubygems'
 require 'nokogiri'
-require 'open-uri'
 
-url = 'http://www.boardgamegeek.com/browse/boardgame'
-path = '#collectionitems tr .collection_objectname'
+url   = File.join(File.dirname(__FILE__), 'boardgamegeek_scrape.html')
+path  = '#collectionitems tr .collection_objectname'
 
-doc = Nokogiri::HTML(open(url))
-puts doc.inspect
-# puts doc.at_css('title').text
-# puts doc.css('#collectionitems').inspect
-# doc.css(path).each do |item|
-#   name = item.at_css('a').text
-#   puts "Name: #{name}"
-# end
+doc = Nokogiri::HTML(File.read(url))
+doc.css(path).each do |item|
+  name = item.at_css('a').text
+  puts "Name: #{name}"
+end
