be7811294ec1d9328c6a4583185523211dad9f1b
Dallas Reedy <code@dallasreedy.com>
2010-03-24 20:36:38 -0700

    migrated the players table

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100325032448) do
+ActiveRecord::Schema.define(:version => 20100325033303) do
 
   create_table "game_plays", :force => true do |t|
     t.integer  "game_id"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(:version => 20100325032448) do
   create_table "games", :force => true do |t|
     t.string   "name"
     t.string   "slug"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "players", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "nickname"
+    t.string   "mo"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,16 +1,12 @@
 require 'spec_helper'
 
 describe Player do
-  before(:each) do
-    @valid_attributes = {
-      :first_name => "value for first_name",
-      :last_name => "value for last_name",
-      :nickname => "value for nickname",
-      :mo => "value for mo"
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    Player.create!(@valid_attributes)
+  it 'should create a new instance given valid attributes' do
+    Player.create!(
+      :first_name => 'Dallas',
+      :last_name  => 'Reedy',
+      :nickname   => 'dallas',
+      :mo         => 'Tile hog!'
+    )
   end
 end
