dc7785313c6068f410827b948f4103ce49b72a24
Dallas Reedy <code@dallasreedy.com>
2010-03-24 20:54:19 -0700

    spec’ing the Player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,16 @@
 class Player < ActiveRecord::Base
+  has_many :rounds
+  has_many :game_plays
+
+  validates_presence_of :first_name, :last_name, :nickname
+  validates_format_of :nickname, :with => /^[a-z-]+$/, :allow_blank => true
+  validates_uniqueness_of :nickname
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def name=(names)
+    self.first_name, self.last_name = names.split(' ', 2)
+  end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -9,4 +9,52 @@ describe Player do
       :mo         => 'Tile hog!'
     )
   end
+
+  it 'should have many rounds' do
+    Player.reflections.should have_key(:rounds)
+    Player.reflections[:rounds].macro.should eql(:has_many)
+  end
+
+  it 'should have many game_plays' do
+    Player.reflections.should have_key(:game_plays)
+    Player.reflections[:game_plays].macro.should eql(:has_many)
+  end
+
+  it 'should require the first_name, last_name and nickname attributes to be present' do
+    player = Player.create
+    player.should have(1).error_on(:first_name)
+    player.should have(1).error_on(:last_name)
+    player.should have(1).error_on(:nickname)
+    player.should have(0).errors_on(:mo)
+  end
+
+  it 'should require the nickname attribute to be in the specified, URL-friendly format' do
+    player = Player.create(
+      :first_name => 'Dallas',
+      :last_name  => 'Reedy',
+      :nickname   => 'dallas reedy'
+    )
+    player.should have(1).error_on(:nickname)
+  end
+end
+
+describe Player, '#name' do
+  it 'should be made up of the first_name and last_name attributes' do
+    player = Player.create(:first_name => 'Dallas', :last_name => 'Reedy')
+    player.name.should eql('Dallas Reedy')
+  end
+end
+
+describe Player, '#name=' do
+  it 'should set the first_name and last_name attributes from one string' do
+    player = Player.create(:name => 'Dallas Reedy')
+    player.first_name.should eql('Dallas')
+    player.last_name.should eql('Reedy')
+  end
+
+  it 'should create exactly two names: the first_name and last_name' do
+    player = Player.create(:name => 'Dallas Adam Reedy')
+    player.first_name.should eql('Dallas')
+    player.last_name.should eql('Adam Reedy')
+  end
 end
